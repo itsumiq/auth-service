5117738b0aeac88c38e97c71787512588ae97008
itsumi <122093200+itsumiq@users.noreply.github.com>
2025-04-09 02:16:07 +0300

    feat(passhash): add functions for hash and check password

---
+++ app/internal/passhash/hash.go
@@ -0,0 +1,16 @@
+package passhash
+
+import "golang.org/x/crypto/bcrypt"
+
+func Hash(password string) (string, error) {
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashPassword), nil
+}
+
+func Verify(hash, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
